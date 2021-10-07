e7db1beecff8acd22b7a87770f2039242f668cd1
ly <ly@ictp.nl>
2021-10-07 08:42:42 +0700

    add course

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import io from 'socket.io-client';
+// const socket = io('http://localhost:3100');
+const socket = io.connect("/");
+export default class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+        };  
+    }
+    componentDidMount() {
+        
+    }
+    render() {
+        return (
+            <div className="container">
+                hello
+            </div>
+        )
+    }
 }
-
-export default App;
